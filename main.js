83fbe18a7e4a15ae1907dc9c3e13bc46857aea75
GinaKoul <129206273+GinaKoul@users.noreply.github.com>
2024-06-28 19:05:43 +0300

    Working with prototypes, will be removed

--- main.js
+++ main.js
@@ -17,3 +17,9 @@ function Book(title,author,pages,read) {
 const theHobbit = new Book('The Hobbit','J.R.R. Tolkien',295,false);
 
 console.log(theHobbit.info());
+console.log(Object.getPrototypeOf(theHobbit) === Book.prototype); // Returns true because theHobbit objects prototype is the Book.prototype
+console.log(Object.getPrototypeOf(theHobbit)); // TheHobbit object inherits from the Book.prototype
+console.log(Object.getPrototypeOf(Book.prototype)); // Book prototype inherits from Object.prototype
+console.log(theHobbit.valueOf()); // The value of method is a method of Object.prototype
+console.log(theHobbit.hasOwnProperty('valueOf')); // Check if the valueOf is a property of theHobbit object
+console.log(Object.prototype.hasOwnProperty('valueOf')); // Check if the valueOf is a property of theHobbit object
