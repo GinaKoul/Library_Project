180638df24220ecdeb4d0cb39554883b872ff6a4
GinaKoul <129206273+GinaKoul@users.noreply.github.com>
2024-10-22 16:44:01 +0300

    Create Book class

--- main.js
+++ main.js
@@ -2,41 +2,67 @@ let myLibrary = [];
 let bookGrid, showButton, addBookDialog, bookTitle, bookAuthor, bookPages, bookRead, addBtn;
 let increasingId = 0;
 
-// Book constructor
-function Book(id,title,author,pages,read) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.titleInfo = function() {
+class Book {
+    constructor(id,title,author,pages,read) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.titleInfo = this.title;
+        this.authorInfo = this.author;
+        this.pagesInfo = this.pages;
+        this.readInfo = this.read;
+        this.card =[this.id,this.titleInfo,this.authorInfo,this.pagesInfo,this.readInfo];
+    }
+
+    get titleInfo() {
+        return this._titleInfo;
+    }
+
+    set titleInfo(value) {
         let bookTitle = document.createElement('h2');
-        bookTitle.textContent = this.title;
-        return bookTitle;
-    };
-    this.authorInfo = function() {
+        bookTitle.textContent = value;
+        this._titleInfo = bookTitle;
+    }
+
+    get authorInfo() {
+        return this._authorInfo;
+    }
+
+    set authorInfo(value) {
         let bookAuthor = document.createElement('p');
         let authorKey = document.createElement('span');
         let authorContent = document.createElement('span');
         authorKey.classList.add('font-bold');
         authorKey.textContent = 'Author: ';
         authorContent.classList.add('font-italic');
-        authorContent.textContent = this.author;
+        authorContent.textContent = value;
         bookAuthor.append(authorKey, authorContent);
-        return bookAuthor;
-    };
-    this.pagesInfo = function() {
+        this._authorInfo = bookAuthor;
+    }
+
+    get pagesInfo() {
+        return this._pagesInfo;
+    }
+
+    set pagesInfo(value) {
         let bookPages = document.createElement('p');
         let pagesKey = document.createElement('span');
         let pagesContent = document.createElement('span');
         pagesKey.classList.add('font-bold');
         pagesKey.textContent = 'Pages: ';
         pagesContent.classList.add('font-italic');
-        pagesContent.textContent = `${this.pages} pages`;
+        pagesContent.textContent = `${value} pages`;
         bookPages.append(pagesKey, pagesContent);
-        return bookPages;
-    };
-    this.readInfo = function() {
+        this._pagesInfo = bookPages;
+    }
+
+    get readInfo() {
+        return this._readInfo;
+    }
+
+    set readInfo(value) {
         let readContainer = document.createElement('div');
         let bookRead = document.createElement('p');
         let readStatusBtn = document.createElement('button');
@@ -44,13 +70,37 @@ function Book(id,title,author,pages,read) {
         bookRead.classList.add('font-bold');
         bookRead.textContent = 'Read';
         readStatusBtn.classList.add('read-switch');
-        readStatusBtn.addEventListener('click',changeStatus);
-        this.read?readStatusBtn.classList.add('read'):null;
+        readStatusBtn.addEventListener('click',this.changeReadStatus);
+        value?readStatusBtn.classList.add('read'):null;
         readContainer.append(bookRead,readStatusBtn);
-        return readContainer;
+        this._readInfo = readContainer;
     }
-    this.changeReadStatus = function() {
-        this.read = this.read ? this.read = false:this.read = true;
+
+    get card() {
+        return this._card;
+    }
+
+    // Create book card
+    set card([id,title,author,pages,read]) {
+        let card = document.createElement('article');
+        let closeButton = document.createElement('button');
+        let closeBtnText = document.createElement('span');
+        closeButton.classList.add('close-btn');
+        closeBtnText.classList.add('sr-only');
+        closeBtnText.textContent = 'Remove from Library';
+        closeButton.appendChild(closeBtnText);
+        closeButton.addEventListener('click',removeBook);
+        card.setAttribute('data-id',id);
+        card.classList.add('book-card');
+        card.append(closeButton,title,author,pages,read);
+        this._card = card;
+    }
+
+    // Toggle the read status of Book class and card
+    changeReadStatus() {
+        this.classList.toggle('read');
+        const currentBook = myLibrary.find((book) => book.id == this.closest('.book-card')?.getAttribute('data-id'));
+        this._read = this._read ? this._read = false:this._read = true;
     }
 }
 
@@ -62,36 +112,13 @@ function removeBook() {
     // showLibrary();
 }
 
-// Toggle the read status of Book object and card
-function changeStatus() {
-    this.classList.toggle('read');
-    const currentBook = myLibrary.find((book) => book.id == this.closest('.book-card')?.getAttribute('data-id'));
-    currentBook.changeReadStatus();
-}
-
 // Display book cards of Library
 function showLibrary() {
     bookGrid.textContent = '';
     myLibrary.forEach(book => {
-        let newBookCard = createBookCard(book);
+        let newBookCard = book.card;
         bookGrid.appendChild(newBookCard);
     })
-}
-
-// Create book card
-function createBookCard(newBook) {
-    let card = document.createElement('article');
-    let closeButton = document.createElement('button');
-    let closeBtnText = document.createElement('span');
-    closeButton.classList.add('close-btn');
-    closeBtnText.classList.add('sr-only');
-    closeBtnText.textContent = 'Remove from Library';
-    closeButton.appendChild(closeBtnText);
-    closeButton.addEventListener('click',removeBook);
-    card.setAttribute('data-id',newBook.id);
-    card.classList.add('book-card');
-    card.append(closeButton,newBook.titleInfo(),newBook.authorInfo(),newBook.pagesInfo(),newBook.readInfo());
-    return card;
 }
 
 // Create new Book using the book constructor and add it to the Library array
